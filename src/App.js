f9629ee342f9d194d8eecc6596a8e466522e79b7
B H An <anbyunghoon0107@gamil.com>
2021-11-20 10:21:52 +0900

    notify.js 생성 / 함수 비동기 로딩

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import notify from './notify';
 
 function App() {
+  const onClick = () => {
+    notify();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p onClick={onClick}>
+          Hello React!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
